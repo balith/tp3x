b2208168fc0d9a4463808ec8a790f75b5e4c8adf
balith <balith@educ.college-montreal.qc.ca>
2022-11-10 10:41:06 -0500

    Initial commit

---
+++ tp3.py
@@ -0,0 +1,66 @@
+import random
+
+
+def restart():
+    recommence = input("voulez vous passer au prochain niveau")
+    if recommence == "oui":
+        return True
+    elif recommence == "non":
+        return False
+
+jouer = True
+while jouer:
+
+    hearts = 20
+
+    forcedemonstre = random.randint(1, 6)
+
+        print(f"il y a un grand monstre avec difficulté de {forcedemonstre}")
+        print("quesceque vous voulez faire")
+        print("1-attaquer le monstre")
+        print("2-contourner l'adversaire")
+        print("3-afficher les regles")
+        print("4-quiter le jeu")
+        option = int(input("choisisser une des quartres options en inserant le nombre qui corespond: "))
+        print()
+
+        if option == 1:
+            chifredudee = random.randint(1,6)
+
+            print(f"vous roulez ler chiffre {chifredudee} ")
+
+            if chifredudee > forcedemonstre:
+                print("le monstre est mort bravo")
+                print(f"il vous reste {hearts + forcedemonstre} de vie")
+
+         elif chifredudee < forcedemonstre:
+         print("vous avez perdu tout votre vie")
+         print(f"il vous reste {hearts - forcedemonstre} de vie")
+
+
+        elif option == 2:
+            print()
+            print("vous decider de contourner le monstre")
+            print("pendant qu'il dohrs mais vous faite tomber une roche")
+            print("lemonstre se reveille et vous attaque just qua votre mort")
+
+
+
+
+        elif option == 3:
+            print()
+            print("vous avec 20 point de vie ")
+            print("si vous choisisser loption 1 vous aller rouler un dee")
+            print("plus le chiffre est haut plus de dommage vous faite")
+            print("le but est de tuer tout les monstre dans votre chemin")
+
+
+
+
+       elif option == 4:
+           print("c'est correct a la prochain fois")
+           jouer = False
+
+        if hearts < 0:
+            if not restart():
+                jouer = False
