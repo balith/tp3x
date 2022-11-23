a8f7d65146a7dbaecc313abcbd1f7396e69dc35b
balith <balith@educ.college-montreal.qc.ca>
2022-11-23 09:13:13 -0500

    Commmit 17 novembre

--- tp3.py
+++ tp3.py
@@ -14,11 +14,11 @@ hearts = 20
 jouer = True
 while jouer:
 
-    forcedemonstre = random.randint(1, 6)
+    force_de_monstre = random.randint(1, 6)
     forcedugrandmonstre = random.randint(10, 20)
     print("")
 
-    print(f"il y a un grand monstre avec difficulté de {forcedemonstre}")
+    print(f"il y a un grand monstre avec difficulté de {force_de_monstre}")
     print("quesceque vous voulez faire")
     print("1-attaquer le monstre")
     print("2-contourner l'adversaire")
@@ -32,12 +32,14 @@ while jouer:
 
         print(f"vous roulez ler chiffre {chifredudee} ")
 
-        if chifredudee > forcedemonstre:
+        if chifredudee > force_de_monstre:
+            hearts += force_de_monstre
             print("le monstre est mort bravo")
-            print(f"il vous reste {hearts + forcedemonstre} de vie")
-        elif chifredudee < forcedemonstre:
+            print(f"il vous reste {hearts} de vie")
+        elif chifredudee < force_de_monstre:
+            hearts -= force_de_monstre
             print("vous avez perdu de la vie")
-            print(f"il vous reste {hearts - forcedemonstre} de vie")
+            print(f"il vous reste {hearts} de vie")
     elif option == 2:
         print()
         print("vous decider de contourner le monstre")
@@ -54,17 +56,17 @@ while jouer:
         print("c'est correct a la prochain fois.")
         jouer = False
 
-    if jouer < 3:
-        chifredudeede20 = random.randint(10, 25)
-
-        print(f"vous roulez ler chiffre {chifredudeede20} ")
-
-        if chifredudeede20 > forcedugrandmonstre:
-            print("le monstre est mort bravo")
-            print(f"il vous reste {hearts + forcedugrandmonstre} de vie")
-        elif chifredudeede20 < forcedugrandmonstre:
-            print("vous avez perdu tout votre vie")
-            print(f"il vous reste {hearts - forcedugrandmonstre} de vie")
+    # elif jouer < 3:
+    #     chifredudeede20 = random.randint(10, 25)
+    #
+    #     print(f"vous roulez ler chiffre {chifredudeede20} ")
+    #
+    #     if chifredudeede20 > forcedugrandmonstre:
+    #         print("le monstre est mort bravo")
+    #         print(f"il vous reste {hearts + forcedugrandmonstre} de vie")
+    #     elif chifredudeede20 < forcedugrandmonstre:
+    #         print("vous avez perdu tout votre vie")
+    #         print(f"il vous reste {hearts - forcedugrandmonstre} de vie")
     if hearts < 0:
         if not restart():
             jouer = False
