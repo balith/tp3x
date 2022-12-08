9debbe21b8ccc4a5561a38a5c4edb0cc675e8fed
balith <balith@educ.college-montreal.qc.ca>
2022-12-08 08:56:48 -0500

    Commmit 8  decembre

--- tp3.py
+++ tp3.py
@@ -13,7 +13,8 @@ hearts = 20
 n_victoir = 0
 jouer = True
 while jouer:
-    if n_victoir < 3:
+    chifre_du_dee_de_20 = random.randint(10, 20)
+    if n_victoir > 3:
         force_du_grand_monstre = random.randint(10, 20)
         if chifre_du_dee_de_20 > force_du_grand_monstre:
             chifre_du_dee_de_20 = random.randint(10, 20)
@@ -26,40 +27,30 @@ while jouer:
             print("vous avez perdu tout votre vie")
             print(f"il vous reste {hearts - force_du_grand_monstre} de vie")
             print(f"vous avez {n_victoir} de victoir")
-    """
-    si victoire == 3
-        generer monstre boss (3-5)
-        afficher info en fonction du type de monstre    
-    sinon
-        generer monstre normal (1-5)
-        afficher info en fonction du type de monstre
-    """
+
     force_de_monstre = random.randint(1, 6)
     print("")
     if n_victoir <= 3:
-        print(f"il y a un grand boss monstre avec difficulté de {force_de_monstre}")
-        print(f"il y a un grand boss monstres avec difficulté de {force_de_monstre}")
-        print("quesceque vous voulez faire")
-        print("1-attaquer le monstre")
-        print("2-contourner l'adversaire")
-        print("3-afficher les regles")
-        print("4-quiter le jeu")
-    elif
         print(f"il y a un monstre avec difficulté de {force_de_monstre}")
-        print(f"il y a un grand boss monstres avec difficulté de {force_de_monstre}")
         print("quesceque vous voulez faire")
         print("1-attaquer le monstre")
         print("2-contourner l'adversaire")
         print("3-afficher les regles")
         print("4-quiter le jeu")
-        option = int(input("choisisser une des quartres options en inserant le nombre qui corespond: "))
+    else:
+        print(f"il y a un grand boss monstres avec difficulté de {force_du_grand_monstre}")
+        print("quesceque vous voulez faire")
+        print("1-attaquer le monstre")
+        print("2-contourner l'adversaire")
+        print("3-afficher les regles")
+        print("4-quiter le jeu")
         print()
 
-
+    option = int(input("choisisser une des quartres options en inserant le nombre qui corespond: "))
+    print()
     if option == 1:
         # different si boss
         chifredudee = random.randint(1, 6)
-        chifre_du_dee_de_20 = random.randint(10, 20)
 
         print(f"vous roulez ler chiffre {chifredudee} ")
 
